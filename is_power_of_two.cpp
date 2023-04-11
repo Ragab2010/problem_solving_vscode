b8edc2d91aebcea81d8484728bf8dfd8e315c3be
Ragab2010 <Ragab2010@github.com>
2023-04-11 03:36:21 +0200

    Create is_power_of_two.cpp

---
+++ is_power_of_two.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+
+
+
+using namespace std;
+bool is_power_of_two(uint32_t number){
+    if(number <=1)
+        return  false;
+    return (number & (number -1 )) == 0;
+}
+
+
+int main(){
+
+    cout<<"---------------------is_power_of_two C , Cpp -----------------"<<endl;
+    for(int i =0 ; i<20; i++){
+        cout<<"number ="<<i << "  is ";
+        bool stauts  = is_power_of_two(i);
+        if(stauts == true){
+            cout<<"is power of 2"<<endl;
+        }else{
+            cout<<"isn't power of 2"<<endl;
+        }
+
+    }
+
+
+    return 0;
+}
