3f35c1da16b0211bce48f39db2e9ce270514ee38
Ragab2010 <Ragab2010@github.com>
2023-04-12 04:51:46 +0200

    Create count_most_consecutive_one_binary.cpp

---
+++ count_most_consecutive_one_binary.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<algorithm>
+#include <bitset>
+
+
+
+using namespace std;
+int count_most_consecutive_one_c(uint32_t number){
+    if(number == 0)
+        return 0;
+    int max=0 , count=0;
+    while(number){
+        
+        if(number & 1){
+            count++;
+            if(count >= max){
+                max = count;
+            }
+        }else{
+            count=0;
+        }
+        number >>=1;
+    }
+    
+    return max;
+}
+int count_most_consecutive_one_v1_c(uint32_t number){
+    if(number == 0)
+        return 0;
+    int max=0;
+    while(number){
+        int count=0;
+        while(number & 1){
+            count++;
+            number >>=1;
+        }
+        if(count >= max){
+            max = count;
+        }
+        number >>=1;
+    }
+    
+    return max;
+}
+
+int count_most_repeated_map_c(int *a  , int size ){
+    if(size  <=1)
+        return -1;
+    int max=0;
+    char seen[256]={0};
+    for (int i = 0; i < size ; i++){
+        seen[a[i]]++;
+        if(seen[a[i]] > max){
+            max=seen[a[i]];
+        }
+    }
+    return max;
+}
+int count_most_consecutive_one_cpp(uint32_t number) {
+    if(number ==0)
+        return 0;
+    std::bitset<sizeof(int)*8> bits(number); // convert the integer to a bitset
+    int max = 0;
+    int count = 0;
+    
+    for (int i = 0; i < bits.size(); i++) {
+        if (bits[i] == 1) {
+            count++;
+            if (count > max) {
+                max = count;
+            }
+        } else {
+            count = 0;
+        }
+    }
+    
+    return max;
+}
+
+int count_most_consecutive_one_v1_cpp(uint32_t number)  {
+    std::bitset<sizeof(int)*8> bits(number); // convert the integer to a bitset
+    
+    int max = 0;
+    int count = 0;
+    
+    for (size_t i = 0; i < bits.size(); i++) {
+        if (bits.test(i)) {
+            count++;
+        } else {
+            max = std::max(max, count);
+            count = 0;
+        }
+    }
+    
+    return std::max(max, count);
+}
+
+
+
+int main(){
+
+    uint32_t number=0x50FF0F6 ; //8
+    cout<<"---------------------count_most_consecutive_one_c-----------------"<<endl;
+    cout<<"the max consecutive one :"<<count_most_consecutive_one_c(number)<<endl;
+
+    cout<<"---------------------count_most_consecutive_one_v1_c-----------------"<<endl;
+    cout<<"the max consecutive one :"<<count_most_consecutive_one_v1_c(number)<<endl;
+
+    cout<<"---------------------count_most_consecutive_one_cpp-----------------"<<endl;
+    cout<<"the max consecutive one :"<<count_most_consecutive_one_cpp(number)<<endl;
+
+    cout<<"---------------------count_most_consecutive_one_v1_cpp-----------------"<<endl;
+    cout<<"the max consecutive one :"<<count_most_consecutive_one_v1_cpp(number)<<endl;
+
+    return 0;
+}
