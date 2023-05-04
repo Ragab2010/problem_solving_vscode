01f9837ec45b9e12c4d94b0e3c42685c39eef317
Ragab2010 <Ragab2010@github.com>
2023-05-04 03:45:55 +0300

    Update show_array_pointers.cpp

--- Embedded/show_array_pointers.cpp
+++ Embedded/show_array_pointers.cpp
@@ -37,11 +37,11 @@ Ans:
 
 Q) What is the output of the below program?
 
-//Assuming the size of the integer is 4 bytes and beginning address of the array is 0.
+Assuming the size of the integer is 4 bytes and beginning address of the array is 0.
 #include <stdio.h>
 int main()
 {
-    //Declare 2D array
+  //  Declare 2D array
     int aiData [3][3]= {1};
     printf("%ld\n\n",aiData);
     printf("%ld\n\n",aiData+1);
@@ -53,7 +53,7 @@ Ans:
 Q) What is the output of the below program?
 
 
-//Assuming the size of the integer is 4 bytes and beginning address of the array is 0.
+Assuming the size of the integer is 4 bytes and beginning address of the array is 0.
 #include <stdio.h>
 int main()
 {
