98b4f7e34866a0d3b8fe95c30aacf171502c1aaa
Ragab2010 <Ragab2010@github.com>
2023-05-04 03:55:45 +0300

    Update get_size_of_array_without_sizeof.cpp

--- get_size_of_array_without_sizeof.cpp
+++ get_size_of_array_without_sizeof.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main(){
     cout<<"---------------------Method:one-----------------"<<endl;
     int array[] = {10, 20, 30, 40, 50, 60};
-    // (&array) is pointer to array 
-    //*(&array) is pointer to int , because * with discard &
-    // *(&array+1)  with increament by size of array , and dereferance the address of the last element
+    // (&array) is pointer to array
+    // (&array+1) here we increment the array by the size of array because array:(*)[6]
+    // *(&array+1) here after we get the address after incremention ,
+    // we dereference the address to convert it from int (*)[6] to int * , 
+    // because:*(&array+1) - array expression has to be the same int *
     int size_of_array = *(&array+1) - array; 
     cout<<"the size of array is :"<<size_of_array<<endl;
     cout<<"---------------------Method:two-----------------"<<endl;
