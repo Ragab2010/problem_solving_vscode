27cf378b80d18a0de0a9d66423cfa4732933b250
Ragab2010 <Ragab2010@github.com>
2023-05-08 20:51:03 +0300

    Create conver_stringArray_to_integerArray.cpp

---
+++ conver_stringArray_to_integerArray.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+
+
+
+using namespace std;
+int * parse_stringArray(char *cstring , int *int_array_size){
+    if(cstring == NULL || int_array_size ==  NULL)
+        return NULL;
+    int len = strlen(cstring);
+    int *int_array = (int *)malloc(sizeof(int)*len);
+    if(int_array ==NULL)
+        return NULL;
+    char *next_number = cstring;
+    int index_intArray=0;
+    int parse_index = 0;
+//loop
+    while(next_number !=NULL && parse_index <len){
+    //discard none numbers 
+    if(!isdigit(*next_number)){
+        //cout<<*next_number<<endl;
+        next_number++;
+    }else{
+        int parse_number =strtol(next_number , &next_number , 10);
+        int_array[index_intArray]=parse_number;
+        index_intArray++;
+        //cout<<parse_number<<endl;
+        //next_number++;
+    } 
+    parse_index++;
+    }
+    int* new_int_array =(int *)realloc(int_array , sizeof(int)* index_intArray);
+    //int* new_int_array = (int*) realloc(int_array, index_intArray * sizeof(int));
+
+    *int_array_size=index_intArray;
+    return new_int_array;
+}
+
+int main(){
+
+    cout<<"---------------------parse_stringArray-----------------"<<endl;
+    char arrString[]= "{1, 23,3 ,4,565, 6,  7 ,8 }";
+    int int_array_size;
+    int *int_array=parse_stringArray(arrString , &int_array_size);
+    for (int i = 0; i < int_array_size; i++){
+        cout<<int_array[i]<<", ";
+    }
+    cout<<endl;
+    free(int_array);
+    return 0;
+}
