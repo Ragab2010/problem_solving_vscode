9a9503b56b180c78afe9ac71ad58926d935c4bb1
Ragab2010 <35271240+Ragab2010@users.noreply.github.com>
2023-09-07 00:18:21 +0300

    Update convert_decimal_to_binary.cpp

--- convert_decimal_to_binary.cpp
+++ convert_decimal_to_binary.cpp
@@ -8,13 +8,33 @@ void convert_decimal_to_binary_onScreen_c(uint32_t number){
     // if(number <=0)
     //     return  false;
     uint32_t shift_number =(sizeof(number) * 8) - 1;
-    while (number){
-        if(number &(1<<shift_number)){
+    uint32_t shift_number_counter =(sizeof(number) * 8);
+    while (shift_number_counter){
+        if(number &(1ul<<shift_number)){
             cout<<"1";
         }else{
             cout<<"0";
         }
-        number <<=1;
+        number <<=1UL;
+        shift_number_counter--;
+    }
+    cout<<endl;
+}
+
+void convert_decimal_to_binary_onScreen_c_v1(uint32_t number){
+    // if(number <=0)
+    //     return  false;
+
+    uint32_t number_digit =(sizeof(number) * 8) - 1; //4byte * 8bit -1
+    uint32_t shift_number =(1ul<<number_digit);
+    
+    while (shift_number){
+        if(number &(shift_number)){
+            cout<<"1";
+        }else{
+            cout<<"0";
+        }
+        shift_number >>=1UL;
     }
     cout<<endl;
 }
@@ -22,15 +42,17 @@ void convert_decimal_to_binary_onScreen_c(uint32_t number){
 char * convert_decimal_to_binary_instring_c(uint32_t number){
 
     char * ptr_binary =(char *)malloc(sizeof(char)*sizeof(number)*8);
-    uint32_t shift_number =(sizeof(number) * 8) - 1;
+
+    uint32_t number_digit =(sizeof(number) * 8) - 1; //4byte * 8bit -1
+    uint32_t shift_number =(1ul<<number_digit);
     char* binary = ptr_binary;
-    while (number){
-        if(number &(1<<shift_number)){
+    while (shift_number){
+        if(number & shift_number){
             *binary='1';
         }else{
             *binary='0';
         }
-        number <<=1;
+        shift_number >>=1UL;
         binary++;
     }
     return ptr_binary;
@@ -47,7 +69,9 @@ string convert_decimal_to_binary_instring_cpp(uint32_t number){
 int main(){
 
     cout<<"---------------------convert_decimal_to_binary_onScreen C , Cpp -----------------"<<endl;
-    convert_decimal_to_binary_onScreen_c(2147483743);
+    convert_decimal_to_binary_onScreen_c(4);
+    cout<<"---------------------convert_decimal_to_binary_onScreen C , Cpp v1 -----------------"<<endl;
+    convert_decimal_to_binary_onScreen_c_v1(4);
 
     cout<<"---------------------convert_decimal_to_binary_instring C , Cpp -----------------"<<endl;
     cout<<convert_decimal_to_binary_instring_c(2147483743)<<endl;
