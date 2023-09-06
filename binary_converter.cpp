06d1ca2a861b390f638c7e2502e7c9aaa5ca4109
Ragab2010 <35271240+Ragab2010@users.noreply.github.com>
2023-09-06 20:47:55 +0300

    Update binary_converter.cpp

--- binary_converter.cpp
+++ binary_converter.cpp
@@ -63,13 +63,16 @@ string BinaryConverter_v2(string str) {
     return "not possible";
 
   const size_t str_len{str.length()};
+  cout<<"the len= "<<str.length()<<endl;
 
   int decimal_number{};
 
-  for (size_t i{str_len}; i >=0; i--) {
+
+  for (size_t i{} , j{str_len-1} ; i <str_len; i++ , j--) {
     if ('0' == str[i])
       continue;
-    decimal_number += 1 << (i );
+    cout<<"str["<<i<<"]="<<str[i]<<endl;
+    decimal_number += (1 << (j));
   }
 
   return to_string(decimal_number);
@@ -89,16 +92,31 @@ string BinaryConverter_v3(string str) {
   return to_string(stoi(str, nullptr, 2));
 }
 
+string BinaryConverter_v4(string str) {
+  str = trim(str);
+
+  if (!is_valid_binary_string(str))
+    return "not possible";
+    /*stoi:convert string to integer ->int stoi (const string&  str, size_t* idx = 0, int base = 10),
+    str -> string
+    idx->Pointer to an object of type size_t, set by the function to position of the next character in str after the numerical value.
+    Numerical base (radix) , 10 , 2 , 0-> zero  for the format sequance 
+    */
+   //to_string-> convert integer tor string
+  return to_string(strtol(str.data() , nullptr , 2));
+}
+
 int main() {
   // cout << BinaryConverter_v1(move(string{gets(stdin)}));
   cout << BinaryConverter_v1(move(string{"101"}))
        << '\n';  // expected output: 5
-  cout << BinaryConverter_v1(move(string{"1000"}))
+  cout << BinaryConverter_v2(move(string{"1000"}))
        << '\n';  // expected output: 8
-  cout << BinaryConverter_v1(move(string{"100101"}))
+  cout << BinaryConverter_v3(move(string{"100101"}))
        << '\n';  // expected output: 37
-  cout << BinaryConverter_v1(move(string{"011"}))
+  cout << BinaryConverter_v4(move(string{"011"}))
        << '\n';  // expected output: 3
+
 
   return 0;
 }
