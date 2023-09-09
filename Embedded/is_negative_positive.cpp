f68d81724196177b566898e3c81e732897826222
Ragab2010 <35271240+Ragab2010@users.noreply.github.com>
2023-09-10 00:25:21 +0300

    Update is_negative_positive.cpp

--- Embedded/is_negative_positive.cpp
+++ Embedded/is_negative_positive.cpp
@@ -6,16 +6,21 @@ using namespace std;
 
 //the MSB IS ONE  when the number is Negative
 bool is_positive(int32_t number){
-    return ( ( number & (1u << (BITS -1 )))  | !number);
+    return !( ( number & (1u << (BITS -1 )))  );
     // not number is for check the ZERO
 }
 
+bool isPositive(int32_t number) {
+    return number > 0;
+}
 
 
 int main(){
 
     cout<<"---------------------is_positive-----------------"<<endl;
-    unsigned int number = -15 ; 
-    cout<<std::boolalpha<<"is_positive=" << is_positive(210)<<endl;
+    int number = 15 ; 
+    cout<<std::boolalpha<<"is_positive=" << is_positive(number)<<endl;
+
+    cout<<std::boolalpha<<"isPositive=" << isPositive(number)<<endl;
     return 0;
 }
