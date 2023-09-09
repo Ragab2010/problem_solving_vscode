2742b548682f3020a4a15668e0da39a4a63664bc
Ragab2010 <35271240+Ragab2010@users.noreply.github.com>
2023-09-09 23:50:07 +0300

    Update swap_first_and_last_digit_of_number.cpp

--- swap_first_and_last_digit_of_number.cpp
+++ swap_first_and_last_digit_of_number.cpp
@@ -8,13 +8,14 @@
 using namespace std;
 
 uint32_t  swap_first_and_last_digit_of_number_c(uint32_t number){
+    //the number is: 12345
     uint32_t temp = number , result=0 , store_swap=0;
     uint32_t first_digit , last_digit;
     if (number == 0)
         return 0;
-    //get last digit
-    last_digit = temp%10;
-    //get first digit
+    //get last digit  --->5
+    last_digit = temp%10; //5
+    //get first digit --->1
     while(temp){
         first_digit = temp%10;
         temp = temp/10;
@@ -22,19 +23,24 @@ uint32_t  swap_first_and_last_digit_of_number_c(uint32_t number){
 
     temp =number;
     temp = temp/10;//drob the last digit
+    //swap the iner number 234
     while((temp/10) != 0){
         store_swap =(store_swap* 10) +(temp % 10);
         temp =temp/10;
     }
+    //print 432
     cout<<"store_swap is "<<store_swap<<endl;
 
+    //result = 0+5 =5
     result =result +last_digit;
-    
+    //result = 5432
     while(store_swap != 0){
         result =(result*10) +(store_swap % 10);
         store_swap =store_swap/10;
     }
+
     //put frist digit 
+    //result = 54321
     result =result * 10;
     result =result +first_digit;
 
