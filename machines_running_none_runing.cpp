3efbb937b70d8b119a939fd800d7218bf32b5880
Ragab2010 <35271240+Ragab2010@users.noreply.github.com>
2023-09-30 17:20:03 +0300

    Update machines_running_none_runing.cpp

--- machines_running_none_runing.cpp
+++ machines_running_none_runing.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <random>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 /*
@@ -51,7 +52,7 @@ int getMinimumOperations(string machines){
             indexZero = i;
             if(countOneBefore > countZeroAfter ){
                 while(indexZero <len && machines[indexZero] =='0' ){
-                        machines[indexZero] =='1';
+                        machines[indexZero] ='1';
                         indexZero++;
                         countZeroAfter--;
                         count++;
@@ -59,7 +60,7 @@ int getMinimumOperations(string machines){
                     }               
             }else{
                 while(indexOne >=0 && machines[indexOne] =='1'){
-                    machines[indexOne] =='0';
+                    machines[indexOne] ='0';
                     indexOne--;
                     countOneBefore--;
                     count++;
@@ -71,10 +72,105 @@ int getMinimumOperations(string machines){
     }
     return count;
 }
+int getMinimumOperations2(string machines) {
+    int len = machines.length();
+    int count = 0;
 
+    for (int i = 0; i < len - 1; i++) {
+        if (machines[i] == '1' && machines[i + 1] == '0') {
+            int countOneBefore = 0;
+            int countZeroAfter = 0;
+            int indexOne = i - 1;
+            int indexZero = i + 1;
+
+            while (indexOne >= 0 && machines[indexOne] == '1') {
+                countOneBefore++;
+                indexOne--;
+            }
+
+            while (indexZero < len && machines[indexZero] == '0') {
+                countZeroAfter++;
+                indexZero++;
+            }
+
+            indexOne = i;
+            indexZero = i;
+
+            if (countOneBefore > countZeroAfter) {
+                while (indexZero < len && machines[indexZero] == '0') {
+                    machines[indexZero] = '1';  // Update machine state
+                    indexZero++;
+                    countZeroAfter--;
+                    count++;
+                }
+            } else {
+                while (indexOne >= 0 && machines[indexOne] == '1') {
+                    machines[indexOne] = '0';  // Update machine state
+                    indexOne--;
+                    countOneBefore--;
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+int getMinimumOperations3(std::string machines) {
+    int n = machines.length();
+    std::vector<int> leftOnes(n, 0);
+    std::vector<int> rightOnes(n, 0);
+
+    // Count the number of '1's to the left of each machine
+    for (int i = 1; i < n; ++i) {
+        leftOnes[i] = leftOnes[i - 1] + (machines[i - 1] == '1');
+    }
+
+    // Count the number of '1's to the right of each machine
+    for (int i = n - 2; i >= 0; --i) {
+        rightOnes[i] = rightOnes[i + 1] + (machines[i + 1] == '1');
+    }
+
+    int minOperations = n; // Initialize with a large value
+
+    for (int i = 0; i < n; ++i) {
+        int flipsNeeded = leftOnes[i] + rightOnes[i];
+        minOperations = std::min(minOperations, flipsNeeded);
+        cout<< flipsNeeded << " "<<minOperations<<endl;
+    }
+
+    return minOperations;
+}
+
+int getMinimumOperations4(std::string machines) {
+    int n = machines.size();
+    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(2, 0));
+
+    // Initialize dp table
+    dp[0][0] = 0; // No operations for the first 0 elements
+    dp[0][1] = 0; // No operations for the first 0 elements
+
+    for (int i = 1; i <= n; ++i) {
+        if (machines[i - 1] == '0') {
+            // If the current machine is '0', we can choose to flip it ('1') or not ('0')
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = std::min(dp[i - 1][0], dp[i - 1][1]) + 1;
+        } else {
+            // If the current machine is '1', we can only choose not to flip it ('1')
+            dp[i][0] = dp[i - 1][0] + 1;
+            dp[i][1] = std::min(dp[i - 1][0], dp[i - 1][1]);
+        }
+    }
+
+    // Return the minimum of operations needed at the end
+    return std::min(dp[n][0], dp[n][1]);
+}
 int main() {
-    string machines = "0010101100";
-    int operations = getMinimumOperations(machines);
-    cout << operations << endl;
+    // string machines = "0010101100";
+    // int operations = getMinimumOperations(machines);
+    string machines2 = "01110010010110001";
+    int operations2 = getMinimumOperations(machines2);
+    cout << operations2 << endl;
 
 }
