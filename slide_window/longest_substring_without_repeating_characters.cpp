dfdf040b4e21d8a6985a46ca684af1ea15ee5100
Ragab2010 <ragabmx2002@gmail.com>
2024-05-03 02:53:23 +0300

    update longest_substring_without_repeating_characters add other solution

--- slide_window/longest_substring_without_repeating_characters.cpp
+++ slide_window/longest_substring_without_repeating_characters.cpp
@@ -20,6 +20,29 @@ int lengthOfLongestSubstring(string s) {
     return maxLength;
 }
 
+
+int lengthOfLongestSubstring(string s) {
+    unordered_set<char> letters;
+    
+    int i = 0;
+    int j = 0;
+    
+    int result = 0;
+    
+    while (j < s.size()) {
+        if (letters.find(s[j]) == letters.end()) {
+            letters.insert(s[j]);
+            result = max(result, j - i + 1);
+            j++;
+        } else {
+            letters.erase(s[i]);
+            i++;
+        }
+    }
+    
+    return result;
+}
+
 int main() {
     // Test cases
     string test1 = "abcabcbb";
